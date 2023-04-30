6810105468f0d60fe1ceedb3433c98bfae62594a
Simon Tepel <53453001+Enceladus2521@users.noreply.github.com>
2023-04-30 16:52:18 +0200

    basic temp splitScreen

---
+++ Assets/__Scripts/Managers/PlayerSpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerSpawnManager : MonoBehaviour
+{
+    
+    
+    private void OnPlayerConnected()
+    {
+        
+    }
+
+    private void OnPlayerDisconnected()
+    {
+        
+    }
+   
+    
+}
