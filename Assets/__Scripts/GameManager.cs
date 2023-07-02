1c9c750d0f0e11fea72bfde2328e048437ec9d47
Drumnicorn <robin@seerig.de>
2023-07-02 16:00:32 +0200

    Random Heavy Obj Gen

--- Assets/__Scripts/GameManager.cs
+++ Assets/__Scripts/GameManager.cs
@@ -117,10 +117,7 @@ public class GameManager : MonoBehaviour
 
     public void Restart()
     {
-        Debug.Log("Restarting");
         SceneManager.LoadScene(1);
-
-        Debug.Log("Restarted");
         GameState.Players.Clear();
         GameState.DeadPlayers.Clear();
 
